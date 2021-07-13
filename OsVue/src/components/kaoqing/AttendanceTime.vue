6ccf611fa0c677b9703d15e4a12303362c21abc8
td11266 <3084393380@qq.com>
2021-07-13 09:30:47 +0800

    考勤信息1

--- OsVue/src/components/kaoqing/AttendanceTime.vue
+++ OsVue/src/components/kaoqing/AttendanceTime.vue
@@ -1,41 +1,8 @@
 <template>
-	<h2 style="margin: 50px 0 0 -100px;">考勤时间表</h2>
-	<el-table :data="tableData" border style="width: 361px ;margin: 20px 0 0 400px;">
-		<el-table-column prop="morningTime" label="上班打卡时间" width="180">
-		</el-table-column>
-		<el-table-column prop="nightTime" label="下班打卡时间" width="180">
-		</el-table-column>
-
-	</el-table>
-
+	<p>考勤时间</p>
 </template>
 
 <script>
-	import {
-		ElMessage
-	} from 'element-plus'
-	export default {
-		data() {
-			return {
-				tableData: [],
-			}
-
-
-		},
-		
-		created() {
-			const _this = this
-			this.axios.get("http://localhost:8088/selectBytime")
-				.then(function(response) {
-					console.log(response)
-					_this.tableData = response.data
-
-				}).catch(function(error) {
-					console.log(error)
-				})
-
-		},
-	}
 </script>
 
 <style>
